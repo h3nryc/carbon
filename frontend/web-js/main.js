b51178711e30c12c204adaf2a5b9f353d9fb94e6
Tom Lister <tomlister3@gmail.com>
2016-08-27 17:31:04 +1000

    lol 2

--- frontend/web-js/main.js
+++
@@ -1,5 +0,0 @@
-var ref = new firebase("https://unum-1f4f8.firebaseio.com/");
-var usersRef = firebase.database().ref('/users/');
-usersRef.on('child_added', function(data) {
-    $('.user-card-inner').append('<div class="user-label">'+data.key+'</div>');
-});
